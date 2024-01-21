ba40bec767d6c99ad47e6e05fd9008ad89efa67a
Andriy <andrijcopek696@gmail.com>
2024-01-21 19:42:03 +0200

    TransactionController

--- Tracker/Controllers/CategoryController.cs
+++ Tracker/Controllers/CategoryController.cs
@@ -27,23 +27,6 @@ namespace Tracker.Controllers
                           Problem("Entity set 'AppDbContext.Categories'  is null.");
         }
 
-        // GET: Category/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
 
         // GET: Category/Create
         public IActionResult AddOrEdit(int id)
@@ -77,23 +60,7 @@ namespace Tracker.Controllers
             return View(category);
         }
 
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
+   
 
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -114,9 +81,5 @@ namespace Tracker.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoryExists(int id)
-        {
-          return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
-        }
     }
 }
