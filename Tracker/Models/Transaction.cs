828599ac51225721d097a1b115bc5bebb306b047
Andriy <andrijcopek696@gmail.com>
2024-01-27 17:20:20 +0200

    All files have been commited

--- Tracker/Models/Transaction.cs
+++ Tracker/Models/Transaction.cs
@@ -7,7 +7,7 @@ namespace Tracker.Models
         [Key]
         public int TransactionId { get; set; }
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public string Note { get; set; } = string.Empty;
         public int Amount { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
